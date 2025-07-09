1443477b5173096a3efb0380941c9e74ff950707
Yousif627 <yousifabdulaziz18@gmail.com>
2025-07-09 04:44:23 -0700

    audio

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,20 @@
 function init() {
 
     const gridELm = document.querySelector('.grid')
+    const scoreElm = document.querySelector('#Score-Display')
+    const audioElem = document.querySelector('#quack')
+
 
     const cells = []
     const gridWidth = 10
     const numberCells = gridWidth * gridWidth
 
     let duckPosition = 3
-
+    let score = 0
+    let totalDucks = 0
     function addDuck() {
         cells[duckPosition].classList.add('duck')
+        totalDucks++
     }
 
 
@@ -19,17 +24,42 @@ function init() {
 
     }
 
+    function endGame() {
+        alert('Game Ended, score: ' + score)
+        score = 0
+        totalDucks = 0
+    }
+
     function playGame() {
 
         setInterval(() => {
-
-            removeDuck()
-            duckPosition = Math.floor(Math.random() * numberCells)
-            addDuck()
-        }, 0.900);
+            if (totalDucks < 10) {
+                removeDuck()
+                duckPosition = Math.floor(Math.random() * numberCells)
+                addDuck()
+            } else {
+                // console.log('game ended')
+                // setTimeout(() => {
+                //     alert('Game ended your score is' + score)
+                // }, 2000)
+            endGame()
+            }
+        }, 1000);
 
     }
 
+    function handleClick() {
+
+        console.log('Handle Click has run!')
+        if (event.target.classList.contains('duck')) {
+            audioElem.pause()
+            audioElem.currentTime = 0
+            score += 100000
+            scoreElm.textContent = `Your score is ${score}!`
+            audioElem.play()
+            console.log(score)
+        }
+    }
 
     function createGrid() {
         // for every cell we require create a div
@@ -38,6 +68,7 @@ function init() {
             const cell = document.createElement('div')
             // cell.classList.add('duck')
             cell.textContent = i
+            cell.addEventListener('click', handleClick)
             cells.push(cell)
             gridELm.appendChild(cell);
         }

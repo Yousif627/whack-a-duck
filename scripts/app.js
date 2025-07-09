564d19ab9e547684f017d2a7b2990c80c4572cdf
Yousif627 <yousifabdulaziz18@gmail.com>
2025-07-09 03:32:32 -0700

    randomize duck

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,53 @@
-function init(){
+function init() {
 
-        const gridELm = document.querySelector('.grid')
+    const gridELm = document.querySelector('.grid')
+
+    const cells = []
+    const gridWidth = 10
+    const numberCells = gridWidth * gridWidth
+
+    let duckPosition = 3
+
+    function addDuck() {
+        cells[duckPosition].classList.add('duck')
+    }
 
 
-    function createGrid(){
+    function removeDuck() {
+
+        cells[duckPosition].classList.remove('duck')
+
+    }
+
+    function playGame() {
+
+        setInterval(() => {
+
+            removeDuck()
+            duckPosition = Math.floor(Math.random() * numberCells)
+            addDuck()
+        }, 0.900);
+
+    }
+
+
+    function createGrid() {
         // for every cell we require create a div
         // append this cell to our grid
-        for (let i=0; i < 100; i++){
+        for (let i = 0; i < numberCells; i++) {
             const cell = document.createElement('div')
+            // cell.classList.add('duck')
             cell.textContent = i
+            cells.push(cell)
             gridELm.appendChild(cell);
-    }
+        }
+
+        console.log(cells)
     }
     createGrid()
-
-
+    // addDuck()
+    // removeDuck()
+    playGame()
 }
 
 document.addEventListener('DOMContentLoaded', init)

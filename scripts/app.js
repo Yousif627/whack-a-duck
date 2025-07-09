67c9c9b71d191988d1c3a98c61a99e88ffa388bf
Yousif627 <yousifabdulaziz18@gmail.com>
2025-07-09 01:14:31 -0700

    create grid

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,18 @@
 function init(){
 
+        const gridELm = document.querySelector('.grid')
+
+
+    function createGrid(){
+        // for every cell we require create a div
+        // append this cell to our grid
+        for (let i=0; i < 100; i++){
+            const cell = document.createElement('div')
+            cell.textContent = i
+            gridELm.appendChild(cell);
+    }
+    }
+    createGrid()
 
 
 }
